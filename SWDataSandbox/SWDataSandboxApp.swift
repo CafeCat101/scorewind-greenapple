ec97e5d8d8f2444533307baee4e1413efaf14129
Leonore Yardimli <leonore1982@gmail.com>
2021-10-23 19:37:51 +0800

    Initial Commit

---
+++ SWDataSandbox/SWDataSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWDataSandboxApp.swift
+//  SWDataSandbox
+//
+//  Created by Leonore Yardimli on 2021/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SWDataSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
